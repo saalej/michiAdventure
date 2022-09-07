e9f217eca5334bb330fb787ddeec13c2729dce0b
Marian Herrera <64176067+saalej@users.noreply.github.com>
2022-09-07 18:56:59 -0500

    Tenemos saltos, que alegría

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-
     public CharacterController controller;
     private Vector3 direction;
     public float speed = 8;
+    public float jumpForce = 10;
+    public float gravity = -20;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +23,20 @@ public class Movement : MonoBehaviour
     {
         float hInput = Input.GetAxis("Horizontal");
         direction.z = hInput * speed;
+
+        bool isGrounded = Physics.CheckSphere(groundCheck.position,0.2f, groundLayer);
+        Debug.Log(isGrounded);
+        direction.y += gravity * Time.deltaTime;
+
+        if (isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+                {
+                    direction.y = jumpForce;
+                }
+        }
+
+        
 
         controller.Move(direction * Time.deltaTime);
     }
